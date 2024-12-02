449063d9070253e97ab40e4c6cd8600294a1be93
Filipp548056 <548056@edu.rocmn.nl>
2024-12-02 16:30:50 +0100

    16:30

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router";
-import container from 'react-bootstrap/container'
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-function Layout() {
-    return(
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/products">Products</Link> | 
+        <Link to="/about">About</Link> | <Link to="/contact">Contact</Link> | 
+        <Link to="/cart">Cart</Link>
+      </nav>
+      <hr />
+      <Outlet /> {}
+      <footer>© 2024 E-Shop</footer>
+    </div>
+  );
+};
 
-
-    )
-}
-
-export default Layout;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import './index.scss'
 import App from './App.jsx'
-
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app";
 import 'bootstrap'
 import { PolylineOutlined, Routes, Router } from '@ricons/material' // De icons die je wilt gebruiken.
 import { Icon } from '@ricons/utils' // De helper component om Icons in te laden, zo kun je met de properties zoals size="" de grootte aanpassen.
@@ -19,15 +20,18 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
   <BrowserRouter>
-  <Routes>
-    <Route element=(<Layout></Layout>)
-    <Route path="/" element={<App />} />
-    <Route path="/contact" element=(<Contact />
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<App />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about" element={<About />} />
+          
+      </Route>
     </Routes>
-    </BrowserRouter>
-
-
-  </Routes>
   </BrowserRouter>
-)
+</React.StrictMode>
+);
+
